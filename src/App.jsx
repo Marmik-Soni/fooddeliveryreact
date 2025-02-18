259aa4251a82983c3f75190732a2bca0bd4bf51d
Marmik-Soni <marmiksoni777@gmail.com>
2025-02-18 20:51:14 +0530

    Removed unnecessary modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./pages/customer/Login";
 import Contact from "./pages/customer/Contact";
@@ -11,7 +9,6 @@ import TeamMember from "./components/customer/TeamMember";
 import Team from "./pages/customer/Team";
 import AddToCart from "./pages/customer/AddToCart";
 import Blogs from "./pages/customer/Blogs";
-
 function App() {
   const [count, setCount] = useState(0);
 
