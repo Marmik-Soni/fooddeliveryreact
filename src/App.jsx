c876d58f54f008f513e5cc56de40deeefdd3d937
Marmik-Soni <marmiksoni777@gmail.com>
2025-02-24 10:32:57 +0530

    format changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+// Libraries imports
 import { useState } from "react";
 import { Routes, Route } from "react-router";
 import "./App.css";
 
-// Customer Pages
+// Customer Pages Imports
 import Login from "./pages/customer/Login";
 import Register from "./pages/customer/Register";
 import Home from "./pages/customer/Home";
@@ -10,13 +11,16 @@ import Contact from "./pages/customer/Contact";
 import Menu from "./pages/customer/Menu";
 import Products from "./pages/customer/Products";
 
-// Admin Pages
+// Admin Pages Imports
 import AdminLogin from "./pages/admin/AdminLogin";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+
+          {/* Client pages Routing */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
@@ -25,7 +29,7 @@ function App() {
         <Route path="/menu" element={<Menu />} />
         <Route path="/products" element={<Products />} />
 
-        {/* Admin signed in */}
+          {/* Admin Pages Routing */}
         <Route path="/admin" element={<AdminLogin />} />
       </Routes>
 
