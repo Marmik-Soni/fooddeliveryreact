f8ed83e279a17c611c8b0c479bdafd58e2fe112b
Marmik-Soni <marmiksoni777@gmail.com>
2025-03-11 11:03:53 +0530

    Added pushing and fetching of DB items

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -5,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey : import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "fooddeliveryreact-a7dd7.firebaseapp.com",
-  projectId: "fooddeliveryreact-a7dd7",
-  storageBucket: "fooddeliveryreact-a7dd7.firebasestorage.app",
-  messagingSenderId: "1056856167323",
-  appId: "1:1056856167323:web:39674ff72d098e13dd473e",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "savorybitesreact.firebaseapp.com",
+  projectId: "savorybitesreact",
+  storageBucket: "savorybitesreact.firebasestorage.app",
+  messagingSenderId: "1098515506081",
+  appId: "1:1098515506081:web:4e1418e5fe245b1be7e1e0"
 };
 
 // Initialize Firebase
